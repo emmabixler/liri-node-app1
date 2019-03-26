4d9b80289f3b6f4d89d2fae135a7293363387e74
leb <leb5313@gmail.com>
2019-03-25 22:21:37 -0400

    added the function for the movie, still testing the functionality of it

--- liri.js
+++ liri.js
@@ -10,8 +10,8 @@ require("dotenv").config();
 //list of varibales
 var keys = require("./keys.js");
 var fs = require("fs");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
 var request = require("request");
@@ -37,7 +37,7 @@ switch (inputRequest) {
   default:
     console.log("hii");
 }
-
+//concert request
 function concertThis(userRequest) {
   var artist = userRequest;
   var url =
@@ -52,3 +52,38 @@ function concertThis(userRequest) {
     }
   });
 }
+
+for (var i = 3; i < inputRequest.length; i++) {
+  if (i > 3 && i < inputRequest.length) {
+    selection = selection + "+" + nodeArgs[i];
+  } else {
+    selection += nodeArgs[i];
+  }
+}
+console.log(selection);
+console.log(command);
+
+if (command == "movie-this") {
+  movie();
+} else if (command == "spotify-this-song") {
+  song();
+}
+
+//movie request
+function movie() {
+  var queryURL =
+    "http:www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=trilogy";
+  axios.get(queryUrl).then(function(response) {
+    console.log(response);
+    console.log("Title: " + response.data.Title);
+    console.log("Release Year: " + response.data.Year);
+    console.log("IMBD ratings: " + response.data.imdbRating);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+    console.log("Rotten Tomatoes:" + response.data.Ratings[1].Value);
+  });
+}
+
+//do what it says
